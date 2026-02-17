55c20c159d2efcfbb7bbaff0cbf6a04c1c104dc9
MOURENDRA <papireddymourendrareddy88@gmail.com>
2026-02-17 16:01:42 +0530

    Base application setup

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+  // Constant variable for Application Name
+  static final String APPLICATION_NAME = "Palindrome Checker App";
+
+  // Constant variable for Application Version
+  static final String APPLICATION_VERSION = "Version 1.0";
+
+  // Main Method - Entry point of the Java application
+  // JVM automatically invokes this method
+  public static void main(String[] args) {
+
+    // Display welcome message
+    System.out.println("==========================================");
+    System.out.println("Welcome to " + APPLICATION_NAME);
+    System.out.println(APPLICATION_VERSION);
+    System.out.println("==========================================");
+
+    // Indicating application flow control
+    System.out.println("Application started successfully.");
+    System.out.println("Ready to proceed to the next use case.");
+    System.out.println("==========================================");
   }
 }
